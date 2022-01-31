de3ee0e11084299e915d01fc53beb8cd0628861d
Corrado.Ferra <corradoferra@gmail.com>
2022-01-31 17:37:29 +0100

    risoluzione problemi

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,36 @@
 <template>
   <div id="app">
-    <Head/>
-    <Main :dischi="dischi"/>
+    <Head />
+    <Main :dischi="dischi" :loading="loading" />
   </div>
 </template>
 
 <script>
-import axios from 'axios'
-import Main from './components/Main.vue'
-import Head from './components/Header.vue'
+import axios from "axios";
+import Main from "./components/Main.vue";
+import Head from "./components/Header.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     Main,
     Head,
   },
   data() {
-    return{
-      dischi: []
-    }
+    return {
+      dischi: [],
+      loading: true,
+    };
   },
-    mounted() {
-        axios.get('https://flynn.boolean.careers/exercises/api/array/music').then((response)=>{
-          this.dischi = response.data.response
-        })
-}
-}
-
+  mounted() {
+    axios
+      .get("https://flynn.boolean.careers/exercises/api/array/music")
+      .then((response) => {
+        this.dischi = response.data.response;
+        this.loading = false;
+      });
+  },
+};
 </script>
 
 <style lang="scss">
@@ -37,6 +40,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,34 +1,44 @@
- <template>
-    <div class="big">
-            <div v-if="dischi.length === 10" class="boxbb">
-                <Card
-                v-for="(disco, index) in dischi" 
-                :key="index" class="card"
-                :immagine="disco.poster"
-                :titolo="disco.title"
-                :autore="disco.author"
-                :anno="disco.year"
-                />
-            </div>
-            <div v-else>
-                <div class="lds-roller"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
-            </div>
-        </div>    
-</template> 
+<template>
+  <div class="big">
+    <div v-if="!loading" class="boxbb">
+      <Card
+        v-for="(disco, index) in dischi"
+        :key="index"
+        class="card"
+        :immagine="disco.poster"
+        :titolo="disco.title"
+        :autore="disco.author"
+        :anno="disco.year"
+      />
+    </div>
+    <div v-else>
+      <div class="lds-roller">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  </div>
+</template>
 
 <script>
-import Card from './Card.vue'
+import Card from "./Card.vue";
 export default {
-    components: {
-        Card,
-    },
-    props: {
-        dischi: Array
-    }
-}
-
+  components: {
+    Card,
+  },
+  props: {
+    dischi: Array,
+    loading : Boolean
+  },
+};
 </script>
 
 <style lang="scss">
-@import '../style/main.scss'
+@import "../style/main.scss";
 </style>
